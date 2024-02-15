be2a8ae7d679e28264f919a372c8d71811d33238
Nicholas Papierniak <nick.papierniak@gmail.com>
2024-02-15 09:26:45 -0600

    Added array and new struct.

--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var assignmentItem =
+            [AssignmentItem(course: "Math", description: "Do problems 1-10", dueDate: Date()),
+             AssignmentItem(course: "English", description: "Read Of Mice Of Men", dueDate: Date()),
+             AssignmentItem(course: "Science", description: "Finish Lab", dueDate: Date())]
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -23,4 +27,10 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+struct  AssignmentItem: Identifiable, Codable {
+    var id = UUID ()
+    var course = String()
+    var description = String()
+    var dueDate = Date()
 }
